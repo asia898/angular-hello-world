2f7b8ba94e8eb98ec253c8b3a77ea911339707b2
asia898 <joanna.kwiatkowska989@gmail.com>
2023-01-26 13:57:50 +0100

    Adding in the comment the alternative way of templating (inline)

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
+  // template: `
+  //   <p>
+  //     hello world works inline!
+  //   </p>
+  // `,
   styleUrls: ['./hello-world.component.css']
 })
 export class HelloWorldComponent {
